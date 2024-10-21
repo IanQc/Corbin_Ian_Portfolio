ad95391b72648b0524439c14c08448f3247eb684
IanQc <ian.corbin.qc@gmail.com>
2024-10-21 16:07:35 -0400

    avancement

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 //jay
 const BurgerButton = document.querySelector("#Burger");
 
+const button_web = document.querySelector(".nouvelles__img--03");
+const button_medieval = document.querySelector(".nouvelles__img--01");
 
+/*
+button_web.addEventListener("click", switchweb);
+button_medieval.addEventListener("click", switchmedieval);
+function switchweb() {
+  window.location.href="ConceptionUnity.html"
+}
+function switchmedieval() {
+  window.location.href="modelisation_medieval.html"
+}*/
 
 const swiperHero = new Swiper('.hero__swipper', {
     // Optional parameters
