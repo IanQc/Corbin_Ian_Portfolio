a6c4bc623b7983b89a309133459aded917744628
IanQc <ian.corbin.qc@gmail.com>
2024-10-23 13:13:20 -0400

    yes

--- main.js
+++ main.js
@@ -49,3 +49,16 @@ const unitySwiper = new Swiper('.unity__swiper', {
 
 
 
+
+const soldat = document.querySelector('.soldat');
+
+soldat.addEventListener('click', function() {
+    window.location.href = 'Animation3D.html'; // Remplacez par l'URL de votre page cible
+});
+
+
+const portail = document.querySelector('.portail');
+
+portail.addEventListener('click', function() {
+    window.location.href = 'portail_halo.html'; // Remplacez par l'URL de votre page cible
+});
